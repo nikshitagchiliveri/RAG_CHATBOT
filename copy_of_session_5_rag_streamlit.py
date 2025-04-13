8e7631da0002ed7de7a0b9aadff769c7f5973393
NIKSHITA GANGADHAR <nikshita.c@somaiya.edu>
2025-04-13 15:02:36 +0530

    Update copy_of_session_5_rag_streamlit.py

--- copy_of_session_5_rag_streamlit.py
+++ copy_of_session_5_rag_streamlit.py
@@ -18,16 +18,16 @@ First, let's install the necessary libraries.
 """
 
 # Install required packages
-!pip install langchain langchain-google-genai langchain_community pypdf chromadb sentence-transformers -q
-!pip install google-generativeai pdfplumber -q
+pip install langchain langchain-google-genai langchain_community pypdf chromadb sentence-transformers -q
+pip install google-generativeai pdfplumber -q
 
 """Next, let's import all required libraries:"""
 
-!pip install streamlit
+pip install streamlit
 
-!pip install pyngrok
+pip install pyngrok
 
-!pip install langchain langchain-google-genai langchain_community pypdf chromadb sentence-transformers -q
+pip install langchain langchain-google-genai langchain_community pypdf chromadb sentence-transformers -q
 
 import os
 import pdfplumber
